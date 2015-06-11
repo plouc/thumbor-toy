af01252f88ec2ed04b388baa520273f39ea3db1a
Raphaël Benitte <benitteraphael@gmail.com>
2015-06-11 22:59:31 +0200

    Publish new release

---
+++ config.js
@@ -0,0 +1,52 @@
+/*
+ * This file is part of thumbor-toy project.
+ *
+ * (c) Raphaël Benitte <thumbor-toy@rbenitte.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+var watermarkImages = [
+];
+
+export default {
+    source: {
+        servers: [
+            {
+                label:  'thumbor demo site',
+                value:  'http://thumbor.thumborize.me/',
+                images: [
+                    { label: 'sample image', value: 'thumborize.me/static/img/beach.jpg' }
+                ]
+            }
+        ]
+    },
+    filters: [
+        'blur',
+        'brightness',
+        'colorize',
+        'contrast',
+        'convolution',
+        'equalize',
+        'extract_focal',
+        'fill',
+        'format',
+        'grayscale',
+        'max_bytes',
+        'noise',
+        'no_upscale',
+        'quality',
+        'rgb',
+        'rotate',
+        'round_corner',
+        'saturation',
+        'sharpen',
+        'strip_icc',
+        {
+            type: 'watermark',
+            settingsConfig: [
+                { choices: watermarkImages }
+            ]
+        }
+    ]
+};
