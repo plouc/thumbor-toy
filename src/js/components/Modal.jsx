89006e1ea433461aec31636290cf2d0f06dedacd
Raphaël Benitte <benitteraphael@gmail.com>
2015-06-11 16:56:55 +0200

    Add animation on settings modal

--- src/js/components/Modal.jsx
+++ src/js/components/Modal.jsx
@@ -6,31 +6,54 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-import React                   from 'react';
-import Reflux                  from 'reflux';
-import PanelsActions           from './../actions/PanelsActions';
-import PanelTypes              from './../stores/PanelTypes';
-import PanelsStore             from './../stores/PanelsStore';
-import ToggleControl           from './form/ToggleControl.jsx';
-import ChoiceControl           from './form/ChoiceControl.jsx';
-import UserPreferencesStore    from './../stores/UserPreferencesStore';
-import UserPreferencesActions  from './../actions/UserPreferencesActions';
-import UserPreferencesTypes    from './../stores/UserPreferencesTypes';
+import React                  from 'react';
+import Reflux                 from 'reflux';
+import PanelsActions          from './../actions/PanelsActions';
+import PanelTypes             from './../stores/PanelTypes';
+import PanelsStore            from './../stores/PanelsStore';
+import ToggleControl          from './form/ToggleControl.jsx';
+import ChoiceControl          from './form/ChoiceControl.jsx';
+import UserPreferencesStore   from './../stores/UserPreferencesStore';
+import UserPreferencesActions from './../actions/UserPreferencesActions';
+import UserPreferencesTypes   from './../stores/UserPreferencesTypes';
+import TweenState             from 'react-tween-state';
 
 
 var Modal = React.createClass({
-    mixins: [Reflux.ListenerMixin],
+    mixins: [
+        Reflux.ListenerMixin,
+        TweenState.Mixin
+    ],
 
     getInitialState() {
         return {
-            opened: PanelsStore.get(PanelTypes.SETTINGS)
+            opened:  PanelsStore.get(PanelTypes.SETTINGS),
+            top:     80,
+            opacity: 0,
+            display: 'none'
         };
     },
 
     componentWillMount() {
         this.listenTo(PanelsStore, () => {
             this.setState({
-                opened: PanelsStore.get(PanelTypes.SETTINGS)
+                opened:  PanelsStore.get(PanelTypes.SETTINGS),
+                display: 'block'
+            });
+            this.tweenState('top', {
+                easing:   TweenState.easingTypes.easeOutBack,
+                duration: 200,
+                endValue: this.state.opened ? 50 : 80,
+                onEnd:    () => {
+                    if (this.state.opened === false) {
+                        this.setState({ display: 'none' });
+                    }
+                }
+            });
+            this.tweenState('opacity', {
+                easing:   TweenState.easingTypes.easeOutBack,
+                duration: 200,
+                endValue: this.state.opened ? 1 : 0
             });
         });
     },
@@ -58,10 +81,15 @@ var Modal = React.createClass({
             { value: 'light', label: 'Light theme' }
         ];
 
+        var style = {
+            top:     this.getTweeningValue('top') + '%',
+            opacity: this.getTweeningValue('opacity')
+        };
+
         return (
-            <div className={classes}>
+            <div className={classes} style={{ display: this.state.display }}>
                 <div className="modal__overlay" onClick={this.onCloseClick}/>
-                <div className="modal__container">
+                <div className="modal__container" style={style}>
                     <div className="panel panel--settings">
                         <h3 className="panel__title">
                             <i className="fa fa-close" onClick={this.onCloseClick}/>
